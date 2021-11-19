da0dd5e90797dc246064f607877f0f9021437a2e
EC2 Default User <ec2-user@ip-172-31-20-160.ec2.internal>
2021-11-19 02:14:26 +0000

    add hitcounter

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ Repo for CDK workshop
 ##### Guide to gitpod https://www.gitpod.io/blog/gitpodify/
 
 ###
-`
-aws lambda invoke --function-name cdk-workshop-HelloHandler2E4FBA4D-BuM04AFQQbI1 out --log-type Tail --query LogResult --output text | base64 -dS
-`
+```bash
+aws lambda invoke --function-name cdk-workshop-HelloHandler2E4FBA4D-BuM04AFQQbI1 out --log-type Tail --query LogResult --output text | base64 -d
+```
--- cdk-workshop/cdk_workshop/cdk_workshop_stack.py
+++ cdk-workshop/cdk_workshop/cdk_workshop_stack.py
@@ -1,30 +1,31 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
     core,
     aws_lambda as _lambda,
-    aws_apigateway as apigw
+    aws_apigateway as apigw,
 )
+
+from .hitcounter import HitCounter
 
 
 class CdkWorkshopStack(core.Stack):
 
-    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
-        
-        my_lambda=_lambda.Function(
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Defines an AWS Lambda resource
+        my_lambda = _lambda.Function(
             self, 'HelloHandler',
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.from_asset('lambda'),
-            handler='hello.handler'
+            handler='hello.handler',
         )
-        
+
+        hello_with_counter = HitCounter(
+            self, 'HelloHitCounter',
+            downstream=my_lambda,
+        )
+
         apigw.LambdaRestApi(
             self, 'Endpoint',
-            handler=my_lambda,
+            handler=hello_with_counter._handler,
         )
-    
-
-        
