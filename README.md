53b46ba92233190eb9c866d883f11e14752291a4
EC2 Default User <ec2-user@ip-172-31-20-160.ec2.internal>
2021-11-19 01:13:02 +0000

    initial commit

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Repo for CDK workshop
 [![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/0ni0nrings/try.aws-cdk.git)
 
 ##### Guide to gitpod https://www.gitpod.io/blog/gitpodify/
+
+###
+`bash
+aws lambda invoke --function-name cdk-workshop-HelloHandler2E4FBA4D-BuM04AFQQbI1 out --log-type Tail --query LogResult --output text | base64 -dS
+`
---
+++ cdk-workshop/cdk_workshop/cdk_workshop_stack.py
@@ -0,0 +1,23 @@
+from aws_cdk import (
+    aws_iam as iam,
+    aws_sqs as sqs,
+    aws_sns as sns,
+    aws_sns_subscriptions as subs,
+    core,
+    aws_lambda as _lambda,
+)
+
+
+class CdkWorkshopStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        
+        my_lambda=_lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler'
+        )
+
+        
